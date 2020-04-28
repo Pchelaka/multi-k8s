110feb18ec8562e4df94c98a90d4e01d11b82146
Pchelaka <destination@bigmir.net>
2020-04-28 17:30:20 +0300

    deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t destination/multi-client:latest -t destination/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t destination/multi-server:latest -t destination/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -r destination/multi-worker:latest -t destination/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push destination/multi-client:latest
+docker push destination/multi-server:latest
+docker push destination/multi-worker:latest
+
+docker push destination/multi-client:$SHA
+docker push destination/multi-server:$SHA
+docker push destination/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=destination/multi-client:$SHA
+kubectl set image deployments/server-deployment server=destination/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=destination/multi-worker:$SHA 
